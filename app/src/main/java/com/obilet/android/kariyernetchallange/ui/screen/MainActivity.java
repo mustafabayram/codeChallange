8d1dfe496b7716fe45289a8d2c9554682311deb6
Mustafa Bayram <mbayram93@gmail.com>
2019-09-23 02:12:13 +0300

    activities created.

---
+++ app/src/main/java/com/obilet/android/kariyernetchallange/ui/screen/MainActivity.java
@@ -0,0 +1,11 @@
+package com.obilet.android.kariyernetchallange.ui.screen;
+
+import com.obilet.android.kariyernetchallange.ui.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/obilet/android/kariyernetchallange/ui/screen/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.obilet.android.kariyernetchallange.ui.screen;
+
+import com.obilet.android.kariyernetchallange.ui.activity.BaseActivity;
+
+/**
+ * Created by Mustafa Bayram on 2019-09-23.
+ */
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
